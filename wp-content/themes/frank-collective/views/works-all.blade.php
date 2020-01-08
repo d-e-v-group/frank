6b4dd78f7d7b0c15a3878628d7235967fc5372da
Melissa DeMund <melikay74@gmail.com>
2020-01-08 13:10:50 -0800

    add featured works to all works page

--- wp-content/themes/frank-collective/views/works-all.blade.php
+++ wp-content/themes/frank-collective/views/works-all.blade.php
@@ -2,7 +2,8 @@
 <?php /* Template Name: Work All */ ?>
 @section('content')
 
-	<?php while ( have_posts() ) : the_post(); ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+
     <div class="root barba-container" data-namespace="archive" data-page-id="Archive" data-transition-in="archive_in" data-transition-out="archive_out" data-page-init="archive_init">
         <div class="main default-content">
             <div class="container">
@@ -37,6 +38,72 @@
                             @endif
                         </div>
                     </div>
+                    
+                        <div class="works-list">
+                            <?php
+                            $options = array(
+                                'post_type' => 'work',
+                                'orderby' => 'post__in',
+                                'post__in' => get_field('works', 8931)
+                            );
+                            $work_idx = 0;
+                            $team = new WP_Query( $options );
+                            while( $team->have_posts() ) : $team->the_post();
+                            ?>
+                            <div class="work-wrap col-md-6">
+                        <div class="work-item position-{{ ($work_idx % 2 === 0) ? 'left' : 'right' }}" data-background-color="{{ (get_field('theme_color')) ? get_field('theme_color') : '#ffffff' }}">
+                            <div class="work view-case-study ajax-load" data-position="{{ ($work_idx % 2 === 0) ? 'left' : 'right' }}" data-link="{{ get_permalink() }}" data-work-item data-appear-offset="0.2">
+
+                                @if(get_field('main_image') || get_field('hero_section_image') || get_field('featured_project_video'))
+                                    @if(get_field('featured_project_video'))
+                                        <div class="featured-project work-video-wrap ajax-preload-assets">
+                                            <video autoplay muted loop playsinline class="work-video">
+                                                <source src="{{ get_field('featured_project_video') }}" type="video/mp4">
+                                            </video>
+                                        </div>
+                                    @else
+                                        <div class="featured-project img" data-image="{{ helper::imageURL(get_field('main_image'), 'full') }}">
+                                            <div class="bg-img" style='background-image: url({{ helper::imageURL(get_field('main_image'), 'full') }});'></div>
+                                        </div>
+                                    @endif
+                                @endif
+                                <div class="desc">
+                                    <h5 class="work-cat">{{ get_field('brand_name') }}</h5>
+                                    @if(get_field('tagline') || get_field('tagline_short'))
+                                        <h5>{{ (get_field('tagline_short')) ? get_field('tagline_short') : get_field('tagline') }}</h5>
+                                    @endif
+                                    <?php
+                                    $term_obj_list = get_the_terms( get_the_ID(), 'service' );
+                                    ?>
+                                    @if(!empty($term_obj_list))
+                                        <?php
+                                        $services = array_chunk($term_obj_list, 3);
+                                        ?>
+                                        <ul class="service-list">
+                                        @foreach($services as $items)
+                                            @foreach($items as $item)
+                                                <li>{{ $item->name }}</li>
+                                            @endforeach
+                                        @endforeach
+                                        </ul>
+                                    @endif
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                    $work_idx++;
+                    endwhile;
+                    wp_reset_postdata();
+                    ?>
+                    </div>
+                            
+                        
+                        
+                    
+
+                    <!-- ALL WORKS -->
                     <?php
                     $args = array(
                         'post_type' => ['work'],
@@ -50,6 +117,9 @@
                     $query_works = new WP_Query( $args );
                     ?>
                     @if($query_works->have_posts())
+
+                    
+
                         <div class="media-group works-all" data-load-more-list="work" data-filter-works-list>
                             <?php
                             while ( $query_works->have_posts() ) : $query_works->the_post();
