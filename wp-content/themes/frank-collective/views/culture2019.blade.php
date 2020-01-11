1020708ff3d777f7690655a95da974c237190277
Brendan Sheehan <feemcgill@gmail.com>
2020-01-10 19:59:30 -0800

    culture page work

--- wp-content/themes/frank-collective/views/culture2019.blade.php
+++ wp-content/themes/frank-collective/views/culture2019.blade.php
@@ -9,7 +9,9 @@
           <?php if (get_field('main_text')): ?>
             <h1><?php the_field('main_text'); ?></h1>
           <?php endif; ?>  
-        
+          <div class="mobile-image">
+            <video src="<?php echo get_template_directory_uri() . '/assets/culture-page/3/MikeJiffy1.mp4' ?>" loop autoplay playsinline muted > 
+          </div>
           <?php if (get_field('secondary_text')): ?>
             <?php the_field('secondary_text'); ?>
           <?php endif; ?>  
@@ -27,16 +29,16 @@
             data-frame-one="<?php echo get_template_directory_uri() . '/assets/culture-page/1/Adomas-Ladder.mp4' ?>" 
             data-frame-two="<?php echo get_template_directory_uri() . '/assets/culture-page/1/CJdog.png' ?>" 
             data-frame-three="<?php echo get_template_directory_uri() . '/assets/culture-page/1/Luke-Peleton.mp4' ?>">
-            <img class="img-holder" style="display:none; position: absolute;  left: 0; top: 0; height: 100%; width: 100%; object-fit: cover;" src="">
-            <video class="vid-holder" style="display:none; position: absolute; left: 0; top: 0; height: 100%; width: 100%; object-fit: cover;" src="" playsinline muted > 
+            <img class="img-holder" src="">
+            <video class="vid-holder" src="" playsinline muted > 
           </div>
 
           <div data-tilt style="height: 300px; width: 392px;" class="culture-image culture-looper img-2" 
             data-frame-one="<?php echo get_template_directory_uri() . '/assets/culture-page/2/AshleyBook.mp4' ?>" 
             data-frame-two="<?php echo get_template_directory_uri() . '/assets/culture-page/2/LAOffice1.png' ?>" 
             data-frame-three="<?php echo get_template_directory_uri() . '/assets/culture-page/2/Mike1.png' ?>">
-            <img class="img-holder" style="display:none; position: absolute;  left: 0; top: 0; height: 100%; width: 100%; object-fit: cover;" src="">
-            <video class="vid-holder" style="display:none; position: absolute; left: 0; top: 0; height: 100%; width: 100%; object-fit: cover;" src="" playsinline muted > 
+            <img class="img-holder" src="">
+            <video class="vid-holder" src="" playsinline muted > 
           </div>    
 
 
@@ -44,21 +46,72 @@
             data-frame-one="<?php echo get_template_directory_uri() . '/assets/culture-page/3/AnnieSophieTeaching.png' ?>" 
             data-frame-two="<?php echo get_template_directory_uri() . '/assets/culture-page/3/Jay-Phone.mp4' ?>" 
             data-frame-three="<?php echo get_template_directory_uri() . '/assets/culture-page/3/MikeJiffy1.mp4' ?>">
-            <img class="img-holder" style="display:none; position: absolute;  left: 0; top: 0; height: 100%; width: 100%; object-fit: cover;" src="">
-            <video class="vid-holder" style="display:none; position: absolute; left: 0; top: 0; height: 100%; width: 100%; object-fit: cover;" src="" playsinline muted > 
+            <img class="img-holder" src="">
+            <video class="vid-holder" src="" playsinline muted > 
           </div>       
 
 
 
-          <div data-tilt style="height: 300px; width: 392px;" class="culture-image culture-looper img-3" 
-            data-frame-one="<?php echo get_template_directory_uri() . '/assets/culture-page/3/AnnieSophieTeaching.png' ?>" 
-            data-frame-two="<?php echo get_template_directory_uri() . '/assets/culture-page/3/Jay-Phone.mp4' ?>" 
-            data-frame-three="<?php echo get_template_directory_uri() . '/assets/culture-page/3/MikeJiffy1.mp4' ?>">
-            <img class="img-holder" style="display:none; position: absolute;  left: 0; top: 0; height: 100%; width: 100%; object-fit: cover;" src="">
-            <video class="vid-holder" style="display:none; position: absolute; left: 0; top: 0; height: 100%; width: 100%; object-fit: cover;" src="" playsinline muted > 
-          </div>       
+          <div data-tilt style="height: 300px; width: 392px;" class="culture-image culture-looper img-4" 
+            data-frame-one="<?php echo get_template_directory_uri() . '/assets/culture-page/4/6game-USE.png' ?>" 
+            data-frame-two="<?php echo get_template_directory_uri() . '/assets/culture-page/4/Annie_1.png' ?>" 
+            data-frame-three="<?php echo get_template_directory_uri() . '/assets/culture-page/4/IMG_1808.png' ?>">
+            <img class="img-holder" src="">
+            <video class="vid-holder" src="" playsinline muted > 
+          </div>  
           
-                                 
+          
+          <div data-tilt style="height: 300px; width: 392px;" class="culture-image culture-looper img-5" 
+            data-frame-one="<?php echo get_template_directory_uri() . '/assets/culture-page/5/AdomasPoster.png' ?>" 
+            data-frame-two="<?php echo get_template_directory_uri() . '/assets/culture-page/5/KeggenPoster.png' ?>" 
+            data-frame-three="<?php echo get_template_directory_uri() . '/assets/culture-page/5/MelindaPoster.png' ?>">
+            <img class="img-holder" src="">
+            <video class="vid-holder" src="" playsinline muted > 
+          </div>   
+          
+          
+          <div data-tilt style="height: 300px; width: 392px;" class="culture-image culture-looper img-6" 
+            data-frame-one="<?php echo get_template_directory_uri() . '/assets/culture-page/6/Frank_Office-Photo_LA_2019.png' ?>" 
+            data-frame-two="<?php echo get_template_directory_uri() . '/assets/culture-page/6/JiffyPeloton.mp4' ?>" 
+            data-frame-three="<?php echo get_template_directory_uri() . '/assets/culture-page/6/Keggen_2.png' ?>">
+            <img class="img-holder" src="">
+            <video class="vid-holder" src="" playsinline muted > 
+          </div>             
+          
+          <div data-tilt style="height: 300px; width: 392px;" class="culture-image culture-looper img-7" 
+            data-frame-one="<?php echo get_template_directory_uri() . '/assets/culture-page/7/Andy-Skateboard.mp4' ?>" 
+            data-frame-two="<?php echo get_template_directory_uri() . '/assets/culture-page/7/Jayteaching.png' ?>" 
+            data-frame-three="<?php echo get_template_directory_uri() . '/assets/culture-page/7/jiffydogs.png' ?>">
+            <img class="img-holder" src="">
+            <video class="vid-holder" src="" playsinline muted > 
+          </div>   
+          
+          
+          <div data-tilt style="height: 300px; width: 392px;" class="culture-image culture-looper img-8" 
+            data-frame-one="<?php echo get_template_directory_uri() . '/assets/culture-page/8/andrewA.png' ?>" 
+            data-frame-two="<?php echo get_template_directory_uri() . '/assets/culture-page/8/keggen-studio.png' ?>" 
+            data-frame-three="<?php echo get_template_directory_uri() . '/assets/culture-page/8/votingstickers.png' ?>">
+            <img class="img-holder" src="">
+            <video class="vid-holder" src="" playsinline muted > 
+          </div>     
+          
+          
+          <div data-tilt style="height: 300px; width: 392px;" class="culture-image culture-looper img-9" 
+            data-frame-one="<?php echo get_template_directory_uri() . '/assets/culture-page/9/CourtneyYoYo.mp4' ?>" 
+            data-frame-two="<?php echo get_template_directory_uri() . '/assets/culture-page/9/Frank_Office-Photo_NY_2019.png' ?>" 
+            data-frame-three="<?php echo get_template_directory_uri() . '/assets/culture-page/9/Projection_IG_4x5_19.png' ?>">
+            <img class="img-holder" src="">
+            <video class="vid-holder" src="" playsinline muted > 
+          </div>     
+          
+          
+          <div data-tilt style="height: 300px; width: 392px;" class="culture-image culture-looper img-10" 
+            data-frame-one="<?php echo get_template_directory_uri() . '/assets/culture-page/10/Book.jpg' ?>" 
+            data-frame-two="<?php echo get_template_directory_uri() . '/assets/culture-page/10/Keggen-Pong.mp4' ?>" 
+            data-frame-three="<?php echo get_template_directory_uri() . '/assets/culture-page/10/WomensMarch.jpg' ?>">
+            <img class="img-holder" src="">
+            <video class="vid-holder" src="" playsinline muted > 
+          </div>               
 
         </div>
       </section>
